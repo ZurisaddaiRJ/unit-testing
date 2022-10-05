e92c2266e950c7c3d08efbe9841671b74f666851
ZurisaddaiRJ <zuleyma.juarez_2002@hotmail.com>
2022-10-05 17:55:16 -0500

    create pow  unit-test #6

---
+++ src/app/pow/pow.spec.ts
@@ -0,0 +1,23 @@
+import { pow } from './pow';
+
+describe('pow', () =>{
+    it('should return 1 if exponent is 0', () =>{
+        const result = pow();
+        expect(result).toBe();
+    })
+
+    it('should return 0 if exponent is negative', () =>{
+        const result = pow();
+        expect(result).toBe();
+    })
+
+    it('should return 8 if base is 2 and exponent is 3', () =>{
+        const result = pow();
+        expect(result).toBe();
+    })
+
+    it('should return 9 if base is 3 and exponent is 2', () =>{
+        const result = pow();
+        expect(result).toBe();
+    })
+})
