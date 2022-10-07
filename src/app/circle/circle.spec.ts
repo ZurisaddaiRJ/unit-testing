dc71713bc9b3260027ca074ec671f20815da933b
ZurisaddaiRJ <zuleyma.juarez_2002@hotmail.com>
2022-10-07 07:21:32 -0500

    success  circle unit-test #7

--- src/app/circle/circle.spec.ts
+++ src/app/circle/circle.spec.ts
@@ -2,27 +2,27 @@ import { circle } from './circle';
 
 describe('circle test suite', () =>{
     it ('should return 0, if radio is 0', () => {
-        const result = circle();
-        expect(result).toBe();
+        const result = circle(0);
+        expect(result).toBe(0);
     })
 
     it ('should return 0, if radio is negative', () => {
-        const result = circle();
-        expect(result).toBe();
+        const result = circle(-1);
+        expect(result).toBe(0);
     })
 
     it ('should return 3.1416 if radio is 1', () => {
-        const result = circle();
-        expect(result).toBe();
+        const result = circle(1);
+        expect(result).toBe(3.1416);
     })
 
     it ('should return 12.5664 if radio is 2', () => {
-        const result = circle();
-        expect(result).toBe();
+        const result = circle(2);
+        expect(result).toBe(12.5664);
     })
 
     it ('should return 28.2744 if radio is 3', () => {
-        const result = circle();
-        expect(result).toBe();
+        const result = circle(3);
+        expect(result).toBe(28.2744);
     })
 })
--- src/app/circle/circle.ts
+++ src/app/circle/circle.ts
@@ -1,3 +1,11 @@
-export function circle() {
-  
+export function circle(r : number) {
+    if (r < 0) {
+        return 0;
+    }
+
+    else {
+        const pi = 3.1416
+        let radio = r * r;
+        return pi * radio;
+    }
 }
