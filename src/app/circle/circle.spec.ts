88ddbae43de93fb47c3d83dbc6d145109080034b
ZurisaddaiRJ <zuleyma.juarez_2002@hotmail.com>
2022-10-06 23:45:14 -0500

    create circle unit-test #7

---
+++ src/app/circle/circle.spec.ts
@@ -0,0 +1,28 @@
+import { circle } from './circle';
+
+describe('circle test suite', () =>{
+    it ('should return 0, if radio is 0', () => {
+        const result = circle();
+        expect(result).toBe();
+    })
+
+    it ('should return 0, if radio is negative', () => {
+        const result = circle();
+        expect(result).toBe();
+    })
+
+    it ('should return 3.1416 if radio is 1', () => {
+        const result = circle();
+        expect(result).toBe();
+    })
+
+    it ('should return 12.5664 if radio is 2', () => {
+        const result = circle();
+        expect(result).toBe();
+    })
+
+    it ('should return 28.2744 if radio is 3', () => {
+        const result = circle();
+        expect(result).toBe();
+    })
+})
