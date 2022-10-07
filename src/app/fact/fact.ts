dcac64ce765b8319afa999ba75b24b2863d6ad96
ZurisaddaiRJ <zuleyma.juarez_2002@hotmail.com>
2022-10-07 10:17:21 -0500

    success  fact  unit-test #8

--- src/app/fact/fact.ts
+++ src/app/fact/fact.ts
@@ -1,3 +1,16 @@
-export function fact() {
-   
+export function fact(x: number) {
+    if(x>0 && x<16){
+        let r: number = x-1;
+        while(r > 0){
+            x=x*r;
+            r=r-1;
+        }
+    }else{
+        if(x<0 || x>16){
+            x = 0;
+        }else{
+            x = 1;
+        }
+    }
+    return x;
 }
